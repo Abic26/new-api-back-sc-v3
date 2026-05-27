02e07f883f6c5152b0df4e96c371b7ddfd942c52
dispositivosjdelectrico2022 <dispositivosjdelectricos2022@gmail.com>
2026-05-27 17:00:58 -0500

    test v2

--- src/services/travel/travelCompletedReportScheduler.js
+++ src/services/travel/travelCompletedReportScheduler.js
@@ -7,8 +7,8 @@ import { Op } from "sequelize";
 const COLOMBIA_TIME_ZONE = "America/Bogota";
 
 // modificar horas en formato 24 horas
-const REPORT_HOUR = 15;
-const REPORT_MINUTE = 12;
+const REPORT_HOUR = 18;
+const REPORT_MINUTE = 2;
 
 let schedulerStarted = false;
 let lastRunDateOnly = null;
