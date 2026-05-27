57e222977b6e966f0cb4a6d2bf0ce0746a2ca313
dispositivosjdelectrico2022 <dispositivosjdelectricos2022@gmail.com>
2026-05-27 12:23:52 -0500

    add conditional to run cronjob v6

--- src/services/travel/travelCompletedReportScheduler.js
+++ src/services/travel/travelCompletedReportScheduler.js
@@ -8,7 +8,7 @@ const COLOMBIA_TIME_ZONE = "America/Bogota";
 
 // modificar horas en formato 24 horas
 const REPORT_HOUR = 12;
-const REPORT_MINUTE = 22;
+const REPORT_MINUTE = 25;
 
 let schedulerStarted = false;
 let lastRunDateOnly = null;
